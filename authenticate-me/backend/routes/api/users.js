7f12b5a1b4087d84d36a98129c57aaf131cd0435
Mikhail <mikhail@LAPTOP-1QUVC2OK.localdomain>
2021-03-25 14:27:54 -0400

    complete signup api setup and error validation for unsuccesful sign up

--- authenticate-me/backend/routes/api/users.js
+++ authenticate-me/backend/routes/api/users.js
@@ -1,19 +1,27 @@
 const express = require('express')
+const asyncHandler = require('express-async-handler');
+
+const { setTokenCookie, requireAuth } = require('../../utils/auth');
+const { User } = require('../../db/models');
+
 const router = express.Router();
 
 
 
+// SIGN UP FUNCTION signs user up calling the sign up static method defined in USER model.
+// if user is created the it sets the cookie and returns JSON response with user info.
+// else it will error into sequelize validation
+router.post( '', asyncHandler(async (req, res) => {
+    const { email, password, username } = req.body;
+    const user = await User.signup({ email, username, password });
 
+    await setTokenCookie(res, user);
 
-
-
-
-
-
-
-
-
-
+    return res.json({
+      user,
+    });
+  }),
+);
 
 
 
