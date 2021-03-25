b7c5c8b8733127b5dd3d43f29b9a2431036b4b9d
Mikhail <mikhail@LAPTOP-1QUVC2OK.localdomain>
2021-03-25 14:43:58 -0400

    completed validation setup for signup

--- authenticate-me/backend/routes/api/users.js
+++ authenticate-me/backend/routes/api/users.js
@@ -1,17 +1,41 @@
 const express = require('express')
 const asyncHandler = require('express-async-handler');
+const { check } = require('express-validator');
+
 
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { User } = require('../../db/models');
+const { handleValidationErrors } = require('../../utils/validation');
+
 
 const router = express.Router();
 
 
+//validations that will ensure signup fields are properly entered and formated.
+const validateSignup = [
+  check('email')
+    .exists({ checkFalsy: true })
+    .isEmail()
+    .withMessage('Please provide a valid email.'),
+  check('username')
+    .exists({ checkFalsy: true })
+    .isLength({ min: 4 })
+    .withMessage('Please provide a username with at least 4 characters.'),
+  check('username')
+    .not()
+    .isEmail()
+    .withMessage('Username cannot be an email.'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .isLength({ min: 6 })
+    .withMessage('Password must be 6 characters or more.'),
+  handleValidationErrors,
+];
 
 // SIGN UP FUNCTION signs user up calling the sign up static method defined in USER model.
 // if user is created the it sets the cookie and returns JSON response with user info.
 // else it will error into sequelize validation
-router.post( '', asyncHandler(async (req, res) => {
+router.post( '', validateSignup, asyncHandler(async (req, res) => {
     const { email, password, username } = req.body;
     const user = await User.signup({ email, username, password });
 
