778a13291827216f126f2f93860cf06bd5d041d5
Mikhail <mikhail@LAPTOP-1QUVC2OK.localdomain>
2021-03-25 14:39:38 -0400

    added validatelogin functionlity to login in function in session.js

--- authenticate-me/backend/routes/api/session.js
+++ authenticate-me/backend/routes/api/session.js
@@ -1,15 +1,30 @@
 const express = require('express')
 const asyncHandler = require('express-async-handler');
+const { check } = require('express-validator');
 
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
+const { handleValidationErrors } = require('../../utils/validation');
+
 
 const router = express.Router();
 
 
 
+// Checks keys for req.body.password and credential.
+const validateLogin = [
+  check('credential')
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage('Please provide a valid email or username.'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a password.'),
+  handleValidationErrors,
+];
+
 //LOG IN FUNCTION logs user in; pulls cred. and pass. from body. if invalid throws errors.
-router.post( '/', asyncHandler(async (req, res, next) => {
+router.post( '/', validateLogin, asyncHandler(async (req, res, next) => {
     const { credential, password } = req.body;
 
     const user = await User.login({ credential, password });
